4e1864ef4d0c5077658b24f3fd8d27c3abc5312f
Maxime AUBLET <maximeaublet@gmail.com>
2019-01-27 19:28:55 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- src/Form/SocietyType.php
+++ src/Form/SocietyType.php
@@ -2,8 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Country;
 use App\Entity\Society;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +15,17 @@ class SocietyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('adress')
-            ->add('city')
-            ->add('cityCode')
-            ->add('country')
-        ;
+            ->add('name', TextType::class)
+            ->add('adress', TextType::class)
+            ->add('city', TextType::class)
+            ->add('cityCode', TextType::class)
+            ->add('country', EntityType::class, [
+                'label' => 'Pays',
+                'class' => Country::class,
+                'choice_label' => 'name',
+                'multiple' => false,
+                'required' => false,
+            ]);;
     }
 
     public function configureOptions(OptionsResolver $resolver)
